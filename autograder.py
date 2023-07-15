865bc354d9e15df602cdb0cd01469ab7ed7cbd58
iamharekrishna <harekrishna6371@gmail.com>
2023-07-15 21:49:42 +0530

    new file added here

---
+++ autograder.py
@@ -0,0 +1,4 @@
+hrs= input("Enter Hours: ")
+rate= input("Enter Rate: ")
+gp = float(hrs)* float(rate) 
+print ("Gross pay is : "+ str(gp))
--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-print ("Hello world")
+# print ("Hello world")
+# x="20"
+# print("age is " + x)
+# print(type(x))
